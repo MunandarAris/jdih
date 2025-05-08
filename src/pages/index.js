ba579ed5c6a33f397cd109ebb682244c75ddf345
Aris Munandar <munandara254@gmail.com>
2025-05-08 15:30:07 +0700

    Init project and ant design

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import { Button } from 'antd';
+import React from 'react';
+
+function Home() {
+	return (
+		<div>
+			<h1>JDIH Polri</h1>
+			<Button color="primary">Submit</Button>
+		</div>
+	);
+}
+
+export default Home;
