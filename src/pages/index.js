c703cc6d36a47e455b108bfece1edce6c2b2156a
Aris Munandar <10238233-MunandarAris@users.noreply.gitlab.com>
2025-05-09 11:50:17 +0700

    Feature/jdihp 2/init table component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,7 @@
-import { Button } from 'antd';
 import React from 'react';
 
 function Home() {
-	return (
-		<div>
-			<h1>JDIH Polri</h1>
-			<Button color="primary">Submit</Button>
-		</div>
-	);
+	return <div></div>;
 }
 
 export default Home;
