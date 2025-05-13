8a3a36a83d27f6887ba0d3c9f38f00a9aff3c7e6
Willy Agustino Efendi <willyagustinoefendi12@gmail.com>
2025-05-13 03:11:28 +0000

    feature<JDIHP-3>:slicing input text

---
+++ src/components/atoms/InputText/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Input, Form } from "antd";
+
+const InputText = ({
+  label = "",
+  name = "",
+  required = false,
+  placeholder = "",
+  onChange = () => {},
+  value = "",
+  error = "",
+  ...props
+}) => {
+  return (
+    <>
+      <Form.Item
+        name={name}
+        validateStatus={error ? "error" : ""}
+        label={
+          label && (
+            <div style={{ fontWeight: 600, color: "#1F1F1F" }}>
+              {label}
+              {required && <span className="required-asterisk">*</span>}
+            </div>
+          )
+        }>
+        <Input
+          placeholder={placeholder}
+          onChange={onChange}
+          value={value}
+          style={{ height: "36px" }}
+          {...props}
+        />
+        {error && (
+          <span
+            style={{
+              color: "red",
+              fontSize: "12px",
+              marginTop: "4px",
+              fontWeight: "normal",
+            }}>
+            {error}
+          </span>
+        )}
+      </Form.Item>
+    </>
+  );
+};
+
+export default InputText;
